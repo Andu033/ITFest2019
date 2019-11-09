31d06607a5a84c10ff6ef9cdacda638166891714
Stanila Andrei <andrei133303@gmail.com>
2019-11-09 10:12:15 +0200

    init

---
+++ src/main/java/com/satrabench/getfriends/GetfriendsApplication.java
@@ -0,0 +1,13 @@
+package com.satrabench.getfriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetfriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetfriendsApplication.class, args);
+	}
+
+}
